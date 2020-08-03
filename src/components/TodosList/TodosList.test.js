066864e2dec9c618f55ae30ae40aa7b23f923236
Pierre Goutheraud <pierre.goutheraud@gmail.com>
2020-08-04 00:48:25 +0200

    Test TodosList

---
+++ src/components/TodosList/TodosList.test.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { createRenderer } from "react-test-renderer/shallow";
+import TestRenderer from "react-test-renderer";
+import TodosList from "./TodosList";
+import Todo from "../Todo/Todo";
+
+const todos = [
+  {
+    userId: 1,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false,
+  },
+
+  {
+    userId: 1,
+    id: 2,
+    title: "quis ut nam facilis et officia qui",
+    completed: true,
+  },
+];
+
+const render = () => {
+  const props = {
+    todos,
+  };
+
+  const renderer = createRenderer();
+  renderer.render(<TodosList {...props} />);
+  const output = renderer.getRenderOutput();
+
+  return {
+    props: props,
+    output: output,
+  };
+};
+
+describe("components", () => {
+  it("should render todos", () => {
+    const { output } = render();
+
+    const todosChildren = output.props.children.props.children;
+    expect(todosChildren.length).toBe(2);
+    todosChildren.forEach((todo, i) => {
+      expect(todo.type).toBe(Todo);
+      expect(Number(todo.key)).toBe(todos[i].id);
+      expect(todo.props.todo).toBe(todos[i]);
+    });
+  });
+});
