1c7b9336d31ef9a8bde5b17df7f46387071ea81c
Pierre Goutheraud <pierre.goutheraud@gmail.com>
2020-08-03 21:39:17 +0200

    Hide StatusBar

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { StatusBar } from "react-native";
 import { Provider } from "react-redux";
 import configureStore from "./src/store/configureStore";
 import Home from "./src/screens/Home";
@@ -8,6 +9,7 @@ const store = configureStore();
 export default function App() {
   return (
     <Provider store={store}>
+      <StatusBar backgroundColor="black" hidden />
       <Home />
     </Provider>
   );
